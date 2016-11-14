3ba74543262046148c76936f083216ad48f66574
Masalkin <masalkin.name@yandex.ru>
2016-11-14 23:53:55 +0500

    Каркас классов и методов

---
+++ Lesson_3/part_1.rb
@@ -0,0 +1,26 @@
+class Station
+  def initialize(name)
+    @name =  name
+  end
+
+  def take_train
+
+  end
+
+  def see_all_trains
+
+  end
+
+  def send_train
+
+  end
+
+end
+
+class Route
+
+end
+
+class Train
+
+end
